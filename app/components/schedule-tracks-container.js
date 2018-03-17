67dc10a58211f945af9e6b9f6396475d89049d45
Chris Masters <chrismasters@gmail.com>
2018-03-17 19:58:59 +0100

    Basic working animation

--- app/components/schedule-tracks-container.js
+++ app/components/schedule-tracks-container.js
@@ -1,5 +1,27 @@
 import Component from '@ember/component';
 
+import move from 'ember-animated/motions/move';
+import { fadeOut, fadeIn } from 'ember-animated/motions/opacity';
+
+import { easeInAndOut } from 'ember-animated/easings/cosine';
+
 export default Component.extend({
-  classNames: ['schedule-tracks-container']
+  classNames: ['schedule-tracks-container'],
+
+  transition: function * ({ keptSprites,
+                            insertedSprites,
+                            removedSprites }) {
+
+    insertedSprites.forEach(fadeIn);
+
+    keptSprites.forEach(sprite => {
+      move(sprite, { easing: easeInAndOut });
+    });
+
+    removedSprites.forEach(sprite => {
+      let endX = sprite.initialBounds.left - sprite.initialBounds.width;
+      sprite.endAtPixel({ x: endX });
+      move(sprite, { easing: easeInAndOut });
+    });
+  }
 });
